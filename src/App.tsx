1627147cf77064061bfe8231eafddc94f4d2ab35
hnsblnbrg <hansibrahimasi@gmail.com>
2022-11-04 23:33:58 +0100

    Update webpack

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import About from './pages/About'
 import Gallery from './pages/Gallery'
-import './index.css'
 
 const App = (): React.ReactElement => {
   return (
