7e95144f0ad3df2b4f0b4cc6e8178127ed852cf1
hnsblnbrg <hansibrahimasi@gmail.com>
2022-11-08 11:51:43 +0100

    Clean up highlight code

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,11 @@ import { NavLink } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import { Squash as Hamburger } from 'hamburger-react';
 
+const highlight = {
+  scale: 0.8,
+  transition: { ease: 'easeIn' },
+};
+
 const Navbar = (): React.ReactElement => {
   return (
     <motion.div
@@ -22,28 +27,13 @@ const Navbar = (): React.ReactElement => {
           </a>
         </div>
         <nav>
-          <motion.li
-            whileTap={{
-              scale: 0.8,
-              transition: { ease: 'easeIn' },
-            }}
-          >
+          <motion.li whileTap={highlight}>
             <NavLink to='/'>Home</NavLink>
           </motion.li>
-          <motion.li
-            whileTap={{
-              scale: 0.8,
-              transition: { ease: 'easeIn' },
-            }}
-          >
+          <motion.li whileTap={highlight}>
             <NavLink to='/gallery'>Gallery</NavLink>
           </motion.li>
-          <motion.li
-            whileTap={{
-              scale: 0.8,
-              transition: { ease: 'easeIn' },
-            }}
-          >
+          <motion.li whileTap={highlight}>
             <NavLink to='/about'>About</NavLink>
           </motion.li>
         </nav>
