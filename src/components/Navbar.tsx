107b0602e2278b4a21f7d07bf9585ac23cc1c11d
hnsblnbrg <hansibrahimasi@gmail.com>
2022-11-08 15:53:41 +0100

    Add initial mobile menu functionality & styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,12 @@ import { motion } from 'framer-motion';
 import { Squash as Hamburger } from 'hamburger-react';
 import NavbarLinks from './NavbarLinks';
 
+const mobile = {
+  initial: { opacity: 0, y: -300 },
+  animate: { opacity: 1, y: 0, transition: { duration: 0.5, ease: 'easeIn' } },
+  exit: { opacity: 0, y: -300, transition: { duration: 0.5, ease: 'easeIn' } },
+};
+
 const Navbar = (): React.ReactElement => {
   const [isOpen, setOpen] = useState(false);
 
@@ -10,11 +16,7 @@ const Navbar = (): React.ReactElement => {
     <motion.div
       initial={{ opacity: 0, y: -180 }}
       animate={{ opacity: 1, y: 0 }}
-      transition={{
-        ease: 'easeInOut',
-        duration: 1,
-        delay: 0.6,
-      }}
+      transition={{ ease: 'easeInOut', duration: 1, delay: 0.6 }}
       className='header'
     >
       <div className='header-content'>
@@ -51,11 +53,21 @@ const Navbar = (): React.ReactElement => {
             toggle={setOpen}
           />
         </div>
-        <div className={'mobile-' + (isOpen ? 'show' : 'hide')}>
-          <div className='mobile-items'>
+        <motion.div
+          variants={mobile}
+          animate={isOpen ? 'animate' : 'exit'}
+          className={'mobile-' + (isOpen ? 'show' : 'hide')}
+        >
+          <motion.div
+            variants={mobile}
+            initial='initial'
+            animate='animate'
+            className='mobile-items'
+            onClick={() => setOpen(false)}
+          >
             <NavbarLinks />
-          </div>
-        </div>
+          </motion.div>
+        </motion.div>
       </div>
     </motion.div>
   );
