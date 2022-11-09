8f70bfdab4a48e86d3fa79db89fc089e4b48e066
hnsblnbrg <hansibrahimasi@gmail.com>
2022-11-09 10:37:04 +0100

    Add initial about section animation

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,17 +3,36 @@ import { motion } from 'framer-motion';
 import React from 'react';
 
 const container = {
-  initial: { opacity: 0, y: 300 },
-  animate: {
+  hidden: { opacity: 0, y: 300, transition: { duration: 0.5 } },
+  show: {
     opacity: 1,
     y: 0,
     transition: {
-      delayChildren: 5.4,
-      staggerChildren: 0.1,
+      staggerChildren: 0.5,
       duration: 0.5,
     },
   },
-  exit: { opacity: 0, y: 300, transition: { duration: 0.5 } },
+  exit: {
+    opacity: 0,
+    y: 300,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
+
+const children = {
+  hidden: {
+    opacity: 0,
+    y: 300,
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.8,
+    },
+  },
 };
 
 const About = (): React.ReactElement => {
@@ -21,42 +40,26 @@ const About = (): React.ReactElement => {
   return (
     <motion.div
       variants={container}
-      initial='initial'
-      animate='animate'
+      initial='hidden'
+      animate='show'
       exit='exit'
       className='about-container'
     >
-      <h1>Hey there!</h1>
-      <motion.div className='about-intro'>
+      <motion.div variants={children}>
+        <h1>Hey there!</h1>
+      </motion.div>
+      <motion.div variants={children} className='about-intro'>
         <p>
           I&apos;m Hansi, an analogue photography enthusiast, going for over 8
           years now.
         </p>
       </motion.div>
-      <motion.div className='about-intro right'>
+      <motion.div variants={children} className='about-intro right'>
         <p>
           Inspired by René Burri, Greg Girard and Sebastião Salgado, legends of
           the craft.
         </p>
       </motion.div>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est eius
-        nesciunt magni. Ipsum asperiores dolore cupiditate quisquam qui eos, non
-        necessitatibus, debitis est atque, beatae tempore at doloribus quo
-        incidunt.Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
-        eius nesciunt magni. Ipsum asperiores dolore cupiditate quisquam qui
-        eos, non necessitatibus, debitis est atque, beatae tempore at doloribus
-        quo incidunt.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Est eius nesciunt magni. Ipsum asperiores dolore cupiditate quisquam qui
-        eos, non necessitatibus, debitis est atque, beatae tempore at doloribus
-        quo incidunt.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Est eius nesciunt magni. Ipsum asperiores dolore cupiditate quisquam qui
-        eos, non necessitatibus, debitis est atque, beatae tempore at doloribus
-        quo incidunt.Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Est eius nesciunt magni. Ipsum asperiores dolore cupiditate quisquam qui
-        eos, non necessitatibus, debitis est atque, beatae tempore at doloribus
-        quo incidunt.
-      </p>
     </motion.div>
   );
 };
