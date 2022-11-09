a5a1d424f87ed5527e6772d343a5e9187eedb284
hnsblnbrg <hansibrahimasi@gmail.com>
2022-11-09 10:52:33 +0100

    Update animation values

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,12 +3,12 @@ import { motion } from 'framer-motion';
 import React from 'react';
 
 const container = {
-  hidden: { opacity: 0, y: 300, transition: { duration: 0.5 } },
+  hidden: { opacity: 0, y: 100, transition: { duration: 0.5 } },
   show: {
     opacity: 1,
     y: 0,
     transition: {
-      staggerChildren: 0.5,
+      staggerChildren: 0.2,
       duration: 0.5,
     },
   },
