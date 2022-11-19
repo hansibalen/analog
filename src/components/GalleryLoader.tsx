519a580c6fbbe5d4c027bbe144a22508dece4e0d
hnsblnbrg <hansibrahimasi@gmail.com>
2022-11-19 16:18:22 +0100

    Add custom preloader for gallery section

---
+++ src/components/GalleryLoader.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+
+const GalleryLoader = (): React.ReactElement => {
+  return <motion.div className='load-img'></motion.div>;
+};
+
+export default GalleryLoader;
--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { motion } from 'framer-motion';
 import { TabTitle } from '../../utils/GeneralFunctions';
+import GalleryLoader from '@src/components/GalleryLoader';
 
 const container = {
   hidden: { opacity: 0, y: 100, transition: { duration: 0.5 } },
@@ -30,30 +31,65 @@ const imageAnim = {
   },
 };
 
+const loader = {
+  loading: {
+    opacity: 1,
+    transition: { duration: 0.2, ease: 'easeInOut' },
+  },
+  hide: {
+    opacity: 0,
+    transition: { duration: 0.2, ease: 'easeOut' },
+  },
+};
+
 const Gallery = (): React.ReactElement => {
   TabTitle('analog | Gallery');
-  const array = [...Array(90).keys()];
+  const imgArray = [...Array(90).keys()];
 
+  const [loading, setLoading] = useState(true);
+  const counter = useRef(0);
+  const imageLoaded = () => {
+    counter.current += 1;
+    if (counter.current >= imgArray.length) {
+      setLoading(false);
+    }
+  };
   return (
-    <motion.div
-      variants={container}
-      initial='hidden'
-      animate='show'
-      exit='exit'
-    >
-      <div className='portfolio'>
-        {array.map((id) => (
-          <motion.img
-            key={id}
-            src={require(`@assets/images/gallery/image-${id}.jpg`)}
-            alt={`image ${id}`}
-            variants={imageAnim}
-            initial='initial'
-            whileInView='view'
-          />
-        ))}
-      </div>
-    </motion.div>
+    <>
+      {/* Loader */}
+      <motion.div
+        className='gallery-loader'
+        style={{ display: loading ? 'block' : 'none' }}
+        variants={loader}
+        initial='hide'
+        animate='loading'
+        exit='hide'
+      >
+        <GalleryLoader />
+      </motion.div>
+      {/* Container */}
+      <motion.div
+        variants={container}
+        initial='hidden'
+        animate='show'
+        exit='exit'
+      >
+        <div className='portfolio'>
+          {imgArray.map((id) => (
+            <motion.img
+              key={id}
+              src={require(`@assets/images/gallery/image-${id}.jpg`)}
+              alt={`image ${id}`}
+              variants={imageAnim}
+              initial='initial'
+              whileInView='view'
+              onLoad={imageLoaded}
+              style={{ display: loading ? 'none' : 'block' }}
+            />
+          ))}
+        </div>
+      </motion.div>
+    </>
   );
 };
 
